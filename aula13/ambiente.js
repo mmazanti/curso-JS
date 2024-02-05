136fb06fd51c43c1dc5b569badd42e78b24fb279
mmazanti <math.mazanti@gmail.com>
2024-02-05 16:36:41 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+while (c <= 0) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} 
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
